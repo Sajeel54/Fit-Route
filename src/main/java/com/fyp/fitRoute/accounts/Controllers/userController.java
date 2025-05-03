2464b924385aa6d26358d7ba411e53d358333e5d
sajeel54 <sajeel54@gmail.com>
2025-05-03 17:08:47 +0500

    followers followings of all users

--- src/main/java/com/fyp/fitRoute/accounts/Controllers/userController.java
+++ src/main/java/com/fyp/fitRoute/accounts/Controllers/userController.java
@@ -58,9 +58,9 @@ public class userController {
         }
     }
 
-    @GetMapping("/followers")
+    @GetMapping("/myfollowers")
     @Operation( summary = "Get your followers" )
-    public ResponseEntity<?> getFollowers(){
+    public ResponseEntity<?> getMyFollowers(){
         try{
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -75,15 +75,45 @@ public class userController {
         }
     }
 
-    @GetMapping("/followings")
+    @GetMapping("/myfollowings")
     @Operation( summary = "Get users whom you follow" )
-    public ResponseEntity<?> getFollowings(){
+    public ResponseEntity<?> getMyFollowings(){
         try{
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             User myProfile = uService.getProfile(authentication, "Profile not found");
 
             return new ResponseEntity<>(
                     flwService.getFollowing(myProfile.getId()),
+                    HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @GetMapping("/followers")
+    @Operation( summary = "Get your followers" )
+    public ResponseEntity<?> getFollowers(@RequestParam String id){
+        try{
+            return new ResponseEntity<>(
+                    flwService.getFollowers(id),
+                    HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @GetMapping("/followings")
+    @Operation( summary = "Get users whom you follow" )
+    public ResponseEntity<?> getFollowings(@RequestParam String id){
+        try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User myProfile = uService.getProfile(authentication, "Profile not found");
+            List<profileCard> users = flwService.getFollowing(id).stream()
+                    .peek(user-> user.setFollow(flwService.checkFollow(myProfile.getId(), user.getId()))).toList();
+            return new ResponseEntity<>(
+                    users,
                     HttpStatus.OK);
 
         } catch (Exception e) {
