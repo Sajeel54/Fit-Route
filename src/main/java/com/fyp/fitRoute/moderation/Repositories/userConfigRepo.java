f4491ba6cb40e4d8f6d4b1a9824c139b4051a16d
sajeel54 <sajeel54@gmail.com>
2025-06-30 23:06:13 +0500

    profile fix

--- src/main/java/com/fyp/fitRoute/moderation/Repositories/userConfigRepo.java
+++ src/main/java/com/fyp/fitRoute/moderation/Repositories/userConfigRepo.java
@@ -2,9 +2,11 @@ package com.fyp.fitRoute.moderation.Repositories;
 
 import com.fyp.fitRoute.notifications.Entity.userConfig;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface userConfigRepo extends MongoRepository<userConfig, String> {
     userConfig findByUsername(String username);
     userConfig findByUserId(String userId);
